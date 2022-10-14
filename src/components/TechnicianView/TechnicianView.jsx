64669b660d5a0a4c688e45faa61eed4aa937e472
Mark Schumacher <schuma1022@gmail.com>
2022-10-14 11:41:13 -0500

    Create base TechnicianView component

---
+++ src/components/TechnicianView/TechnicianView.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import LogOutButton from '../LogOutButton/LogOutButton';
+import { useSelector } from 'react-redux';
+
+function TechnicianView() {
+    const user = useSelector((store) => store.user);
+    return (
+        <div className="container">
+            <h2>Welcome to the technician page, {user.username}!</h2>
+            <p>Your ID is: {user.id}</p>
+            <p>Your position is: {user.position}</p>
+            <LogOutButton className="btn" />
+        </div>
+    );
+};
+
+// this allows us to use <App /> in index.js
+export default TechnicianView;
