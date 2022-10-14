b043b76b2fd14e1e04b40e0a12398e2f952830c0
Mark Schumacher <schuma1022@gmail.com>
2022-10-14 12:07:08 -0500

    Create base profile view page

---
+++ src/components/ProfileView/ProfileView.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import LogOutButton from '../LogOutButton/LogOutButton';
+import { useSelector } from 'react-redux';
+
+function ProfileView() {
+    const user = useSelector((store) => store.user);
+    return (
+        <div className="container">
+            <h2>Welcome to the profile page, {user.username}!</h2>
+            <p>Your ID is: {user.id}</p>
+            <p>Your position is: {user.position}</p>
+            <p>Your first name is: {user.first_name}</p>
+            <p>Your first name is: {user.last_name}</p>
+            inputs to PUT /api/user here
+            <LogOutButton className="btn" />
+        </div>
+    );
+};
+
+// this allows us to use <App /> in index.js
+export default ProfileView;
