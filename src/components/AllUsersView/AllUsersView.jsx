0fbc4056d7854ba1a6c6c6400addb1a800fb659e
Mark Schumacher <schuma1022@gmail.com>
2022-10-27 12:32:32 -0500

    Add confirmation to delete user

--- src/components/AllUsersView/AllUsersView.jsx
+++ src/components/AllUsersView/AllUsersView.jsx
@@ -11,10 +11,17 @@ import Avatar from '@mui/material/Avatar';
 import Divider from '@mui/material/Divider';
 import technicianAvatar from '../../images/technician.jpg'
 import businessAvatar from '../../images/dealership.jpg'
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 function AllUsersView() {
     const user = useSelector((store) => store.user);
     const [allUsers, setAllUsers] = useState([])
+    const [alertStatus, setAlertStatus] = useState(false);
+    const [userToDelete, setUserToDelete] = useState(0);
 
     useEffect(() => {
         getAllUsers();
@@ -35,6 +42,7 @@ function AllUsersView() {
         console.log('In deleteUser');
         axios.delete(`/api/user/${userId}`)
             .then(() => {
+                setAlertStatus(false);
                 getAllUsers();
             }).catch((error) => {
                 console.log(error);
@@ -42,9 +50,34 @@ function AllUsersView() {
             });
     };
 
+    const updateAlert = (userId) => {
+        setUserToDelete(userId);
+        setAlertStatus(true);
+    };
+
     return (
         <div className="container">
-           <List
+
+            <Dialog
+                open={alertStatus}
+                onClose={() => setAlertStatus(false)}
+            >
+                <DialogTitle >
+                    Are you sure you want to delete this user?
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText >
+                        This CANNOT be undone!
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button variant="contained" color="error" onClick={() => deleteUser(userToDelete)} autoFocus>
+                        DELETE
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+            <List
                 sx={{
                     width: '100%',
                     maxWidth: 360,
@@ -71,6 +104,9 @@ function AllUsersView() {
             >
                 {
                     allUsers.map(user => {
+
+
+
                         return (
                             <>
                                 <ListItem>
@@ -78,7 +114,7 @@ function AllUsersView() {
                                         <Avatar src={user.position === 'Technician' ? technicianAvatar : businessAvatar} />
                                     </ListItemAvatar>
                                     <ListItemText primary={user.first_name + ' ' + user.last_name} secondary={user.classification} />
-                                    <Button color="error" variant="outlined" onClick={() => deleteUser(user.id)}>Delete</Button>
+                                    <Button color="error" variant="outlined" onClick={() => updateAlert(user.id)}>Delete</Button>
                                 </ListItem>
                                 <Divider variant="inset" component="li" />
                             </>
