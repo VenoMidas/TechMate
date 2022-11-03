ad533515e24b90026a031db8ab08a6ca344ef336
Mark Schumacher <schuma1022@gmail.com>
2022-11-02 19:56:17 -0500

    Update database name

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -13,6 +13,7 @@ let config = {};
 if (process.env.DATABASE_URL) {
   // Heroku gives a url, not a connection object
   // https://github.com/brianc/node-pg-pool
+  // postgresql://VenoMidas:v2_3vGuB_p4v7LGcdHUbU4cNpfxAQzCs@db.bit.io/VenoMidas/tech_mate
   const params = url.parse(process.env.DATABASE_URL);
   const auth = params.auth.split(':');
 
@@ -21,7 +22,7 @@ if (process.env.DATABASE_URL) {
     password: auth[1],
     host: params.hostname,
     port: params.port,
-    database: params.pathname.split('/')[1],
+    database: process.env.DATABASE_NAME || params.pathname.split('/')[1],
     ssl: { rejectUnauthorized: false },
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
